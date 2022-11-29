8c29fa02a4f2815a11e496c44cb0f760d244834d
gg-mike <michal.g.grygiel@gmail.com>
2022-11-29 12:50:43 +0100

    Add logging to auth Lambda

--- src/lambda/auth.py
+++ src/lambda/auth.py
@@ -1,26 +1,34 @@
 import requests
+from utility import get_access_token
+
+
+TOKEN_VERIFIER_URL = "https://www.googleapis.com/oauth2/v1/tokeninfo?access_token="
 
 
 def isAuthorized(headers):
     """Check if request is valid."""
 
     # Debug mode
-    if headers.get("Debug") == True:
+    if headers.get("Debug") is not None:
+        print("!!! DEBUG MODE ENABLED !!!")
         return True
 
     # TODO: in future this function should ensure that request was made from application
     try:
-        token = headers["Authorization"].split(" ")[1]
-        response = requests.get(
-            f"https://www.googleapis.com/oauth2/v1/tokeninfo?access_token={token}"
-        )
-        if response.status_code == 200:
-            return True
+        token, err = get_access_token(headers)
+        if err is not None:
+            print(f"Encountered error while retrieving token: {err}")
+            return False
+
+        print("Processing token")
+        code = requests.get(f"{TOKEN_VERIFIER_URL}{token}").status_code
+        print(f"Return code for given token: {code}")
+        return code == 200
     except:
         return False
 
-    return False
-
 
 def handler(event, context):
-    return {"isAuthorized": isAuthorized(event["headers"])}
+    response = {"isAuthorized": isAuthorized(event["headers"])}
+    print(f"Response: {response}")
+    return response
