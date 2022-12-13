5a7c6993aa4aa8ffbc57d0014f345fc85526b81b
Kacper <kacper-cyganik@o2.pl>
2022-12-13 20:59:36 +0100

    Create AddCulturalEventToCalendar intence handling.

--- src/lambda/cultural_events.py
+++ src/lambda/cultural_events.py
@@ -29,7 +29,7 @@ def validate_user_input(slots: dict) -> dict:
     """
 
     date = slots.get("Date", None)
-
+    cultural_event_index = slots.get("CulturalEventIndex", None)
     if date:
         if not isvalid_date(date):
             return build_validation_result(
@@ -39,7 +39,11 @@ def validate_user_input(slots: dict) -> dict:
             return build_validation_result(
                 False, "Date", "That date is in the past. Provide future date"
             )
-
+    if cultural_event_index:
+        if not isinstance(cultural_event_index, int):
+            return build_validation_result(
+                False, "CulturalEventIndex", "Provided index is not an integeer number. Provide valid index."
+            )
     return {"isValid": True}
 
 
@@ -62,7 +66,8 @@ def get_cultural_events_by_city(intent_request: dict) -> dict:
 
     if intent_request["invocationSource"] == "DialogCodeHook":
         # Validate any slots which have been specified.  If any are invalid, re-elicit for their value
-        validation_result = validate_user_input(intent_request["currentIntent"]["slots"])
+        validation_result = validate_user_input(
+            intent_request["currentIntent"]["slots"])
         if not validation_result["isValid"]:
             slots = intent_request["currentIntent"]["slots"]
             slots[validation_result["violatedSlot"]] = None
@@ -77,16 +82,17 @@ def get_cultural_events_by_city(intent_request: dict) -> dict:
         return delegate(session_attributes, slots)
 
     if source == "FulfillmentCodeHook":
-        logger.debug("FulfillmentCodeHook activated")
 
         date = slots.get("Date", None)
         city = slots.get("City", None)
         response_message = ""
 
-        response = events_table.query(KeyConditionExpression=Key("location").eq((city)))
+        response = events_table.query(
+            KeyConditionExpression=Key("location").eq((city)))
         items = response["Items"]
         if date:
-            items = [item for item in items if item.get("date_start", None) == date]
+            items = [item for item in items if item.get(
+                "date_start", None) == date]
         else:
             # get ongoing events for next week
             response_message += "No date provided, getting events for next week"
@@ -102,7 +108,8 @@ def get_cultural_events_by_city(intent_request: dict) -> dict:
 
         if items:
             for count, item in enumerate(items):
-                session_attributes[f'cultural_event_{count+1}']=json.dumps(item)
+                session_attributes[f'cultural_event_{count+1}'] = json.dumps(
+                    item)
                 response_message += f"{count+1}) Event name: {item.get('event_name','no title')}\n "
                 if item.get("time_start", None):
                     response_message += f"starts at {item.get('date_start','no date specified')} {item['time_start']}\n "
@@ -112,8 +119,6 @@ def get_cultural_events_by_city(intent_request: dict) -> dict:
                     )
                 if item.get("link", None):
                     response_message += f"read more: {item['link']}\n "
-                
-
 
         else:
             response_message = f"There are no ongoing events in {city}"
@@ -131,13 +136,49 @@ def get_cultural_events_by_city(intent_request: dict) -> dict:
 
 
 def add_cultural_event_to_calendar(intent_request: dict) -> dict:
+    source = intent_request.get("invocationSource", None)
+    slots = intent_request["currentIntent"]["slots"]
+
     session_attributes = intent_request.get("sessionAttributes", {})
-    logger.debug(f"add_cultural_event_to_calendar sessionAttributes {session_attributes}")
-    return close(
-        session_attributes,
-        "Fulfilled",
-        {"contentType": "PlainText", "content": "OK"},
-    )
+    logger.debug(f"source {source}")
+    logger.debug(f"slots {slots}")
+
+    if intent_request["invocationSource"] == "DialogCodeHook":
+        # Validate any slots which have been specified.  If any are invalid, re-elicit for their value
+        validation_result = validate_user_input(
+            intent_request["currentIntent"]["slots"])
+        if not validation_result["isValid"]:
+            slots = intent_request["currentIntent"]["slots"]
+            slots[validation_result["violatedSlot"]] = None
+
+            return elicit_slot(
+                session_attributes,
+                intent_request["currentIntent"]["name"],
+                slots,
+                validation_result["violatedSlot"],
+                validation_result["message"],
+            )
+        return delegate(session_attributes, slots)
+
+    if source == "FulfillmentCodeHook":
+        logger.debug(f"session attributes {session_attributes}")
+        cultural_event_index = slots.get("CulturalEventIndex", None)
+        cultural_event_json = session_attributes.get(
+            f"cultural_event{cultural_event_index}", None)
+        if not cultural_event_json:
+            return close(
+                session_attributes,
+                "Fulfilled",
+                {"contentType": "PlainText", "content": f"sorry, couldn't add cultural event ({cultural_event_json}) to calendar."})
+        cultural_event = json.loads(cultural_event_json)
+
+        logger.debug(f"cultural event with index ({cultural_event}) {cultural_event}")
+
+        return close(
+            session_attributes,
+            "Fulfilled",
+            {"contentType": "PlainText", "content": response_message},
+        )
 
 
 def dispatch(intent_request: dict) -> dict:
@@ -163,8 +204,8 @@ def dispatch(intent_request: dict) -> dict:
     # Dispatch to your bot's intent handlers
     if intent_name == "GetCulturalEventsCity":
         return get_cultural_events_by_city(intent_request)
-    # elif intent_name == "AddCulturalEventToCalendar":
-    #     return add_cultural_event_to_calendar(intent_request)
+    elif intent_name == "AddCulturalEventToCalendar":
+        return add_cultural_event_to_calendar(intent_request)
 
     raise Exception("Intent with name " + intent_name + " not supported")
 
